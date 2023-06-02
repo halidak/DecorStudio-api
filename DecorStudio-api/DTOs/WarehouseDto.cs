0179138d1762fd4f5413761d30bb76aeffc65a1c
halidak <halida.karisik6@gmail.com>
2023-06-02 12:18:11 +0200

    models added

---
+++ DecorStudio-api/DTOs/WarehouseDto.cs
@@ -0,0 +1,12 @@
+﻿using DecorStudio_api.Models;
+
+namespace DecorStudio_api.DTOs
+{
+    public class WarehouseDto
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double Size { get; set; }
+        public int StoreId { get; set; }
+    }
+}
